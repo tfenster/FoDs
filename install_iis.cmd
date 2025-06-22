bd3da07bdee161de1e62ae9cc8e7088f7180729e
Tobias Fenster <tfenster@4psbau.de>
2025-06-22 20:01:46 +0200

    Create install_iis.cmd

---
+++ install_iis.cmd
@@ -0,0 +1,5 @@
+DISM /Online /Add-Capability /CapabilityName:Microsoft.NanoServer.IIS /NoRestart
+if errorlevel 3010 (
+    echo The specified optional feature requested a reboot which was suppressed.
+    exit /b 0
+)
